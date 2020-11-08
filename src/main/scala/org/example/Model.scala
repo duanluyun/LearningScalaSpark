e89efed89956d90b7b76e1347e1b5301e23a4153
duanluyun <duanluyunemail@126.com>
2020-11-08 15:30:38 +0800

    V2.0.0DLY0401构建主构造函数

--- src/main/scala/org/example/Model.scala
+++ src/main/scala/org/example/Model.scala
@@ -1,6 +1,4 @@
 package org.example
-
-
 import org.apache.spark.sql.SparkSession
 
 object Model {
@@ -8,5 +6,34 @@ object Model {
 
   def main(args: Array[String]): Unit = {
 
+    val p=new Person("luyun","duan")
+
+    println(p.age)
+
+    p.firstname="wenqi"
+    p.lastname="cheng"
+   print(p)
+
   }
+}
+
+
+class Person( var firstname:String,var lastname:String){
+
+
+  val age=0
+
+  override def toString=s"the lastname is $lastname,the first name is $firstname,the age is $age"
+  private val home:String=System.getProperty("user.dir")
+
+
+  def printHome{println(home)}
+
+
+  def printTotalName{println(this)}
+
+  printHome
+  printTotalName
+
+
 }
